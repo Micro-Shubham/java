f588529ec587ab7c8ed8b01ff1d428568fa41d6c
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-21 20:02:04 +0530

    else if

--- src/Basic02/elseIf.java
+++ src/Basic02/elseIf.java
@@ -1,4 +1,23 @@
 package Basic02;
 
+import java.util.Scanner;
+
 public class elseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day number ");
+        int day = sc.nextInt();
+        if(day == 1) {
+            System.out.println("Its mondey");
+        } else if(day ==2) {
+            System.out.println("Its Tuesday");
+        } else if(day ==3) {
+            System.out.println("Its Wednesday");
+        } else if(day ==  4) {
+            System.out.println("Its Thrusday");
+        } else {
+            System.out.println("That's wrong input");
+        }
+
+    }
 }
