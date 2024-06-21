f2b5c6c031aff9925dfaf824e8112447247c1090
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-21 23:00:35 +0530

    forloop

--- src/Basic02/forLoop.java
+++ src/Basic02/forLoop.java
@@ -1,4 +1,15 @@
 package Basic02;
 
+import java.util.Scanner;
+
 public class forLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int table = sc.nextInt();
+        for(int i = 1 ; i <=10; i++) {
+            int mult = table * i ;
+            System.out.println(mult);
+        }
+    }
 }
