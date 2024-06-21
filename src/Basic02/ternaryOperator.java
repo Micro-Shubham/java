cd4cb65edc28fa875530c5c98e9f32f1e7ca28e5
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-21 22:07:13 +0530

    ternary operator

--- src/Basic02/ternaryOperator.java
+++ src/Basic02/ternaryOperator.java
@@ -1,4 +1,11 @@
 package Basic02;
 
 public class ternaryOperator {
+    public static void main(String[] args) {
+        //ternary operator
+        int a = 23;
+        int b = 56;
+        int max = a>b ? a:b;
+        System.out.println("The max value is " + max);
+    }
 }
