52075e394239b60fbd06f018df14b047e928d80c
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-22 00:06:37 +0530

    array

--- src/array/array.java
+++ src/array/array.java
@@ -1,4 +1,18 @@
 package array;
 
 public class array {
+    public static void main(String[] args) {
+        //declaration of array in java
+        int age[];
+        //memory allocation
+        age = new int[5];
+        //or
+        int table[] = new int[10];
+        for(int i = 0 ;i<10; i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
 }
