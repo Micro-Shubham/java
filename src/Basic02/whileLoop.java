53c99a1fd581901e6d7d519bdc5def5b80c3252f
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-21 23:06:35 +0530

    while

--- src/Basic02/whileLoop.java
+++ src/Basic02/whileLoop.java
@@ -1,4 +1,18 @@
 package Basic02;
 
+import java.util.Scanner;
+
 public class whileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in );
+        System.out.println("Enter the number ");
+        int table = sc.nextInt();
+        int i = 1;
+        while(i<10)
+        {
+            int mul = table * i;
+            System.out.println(mul);
+            i++;
+        }
+    }
 }
