dc1952a86e92d8b3aa5d0c61cca1a9d723a22bdb
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-21 23:13:02 +0530

    doWhile

--- src/Basic02/doWhile.java
+++ src/Basic02/doWhile.java
@@ -1,4 +1,17 @@
 package Basic02;
 
+import java.util.Scanner;
+
 public class doWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in );
+        System.out.println("Enter the number ");
+        int table = sc.nextInt();
+        int i =1 ;
+        do{
+            int mul = table *i;
+            System.out.println(mul);
+            i++;
+        } while(i <= 10);
+    }
 }
