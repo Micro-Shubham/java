e73739c6cedd0f483e9119d48312bfd3ba0ade9c
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-22 16:21:15 +0530

    math

--- src/methods/maths.java
+++ src/methods/maths.java
@@ -1,4 +1,10 @@
 package methods;
 
 public class maths {
+    public static void main(String[] args) {
+        int a = 5, b=9;
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.max(a,b));
+    }
+
 }
