5c5256a510eab0edfc3a0d5a198a089f7b6423b9
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-22 12:35:33 +0530

    forEach loop in array

--- src/array/forEach.java
+++ src/array/forEach.java
@@ -1,4 +1,11 @@
 package array;
 
 public class forEach {
+    public static void main(String[] args) {
+        String name[] = {"Ram", "Shyam", "Radhe", "sita"};
+       for(String temp : name) {
+           System.out.println(temp);
+       }
+
+    }
 }
