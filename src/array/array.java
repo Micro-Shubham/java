97c0f7c6f3b80efd1c4ecbca11b15c539385078e
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-22 12:27:01 +0530

    array

--- src/array/array.java
+++ src/array/array.java
@@ -10,8 +10,9 @@ public class array {
         int table[] = new int[10];
         for(int i = 0 ;i<10; i++)
         {
-            System.out.println(i);
+            System.out.println(i); 
         }
+        System.out.println("The length of array "+ table.length);
 
 
     }
--- src/array/array01.java
+++ src/array/array01.java
@@ -1,4 +1,8 @@
 package array;
 
 public class array01 {
+    public static void main(String[] args) {
+        int marks[] = {98, 93, 23, 46};
+        System.out.println(marks[3]);
+    }
 }
