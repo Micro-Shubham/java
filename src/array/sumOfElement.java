1b55a87938cbb0bafc1aa97d001b5372c059488f
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-22 12:39:51 +0530

    sumOfElement

--- src/array/sumOfElement.java
+++ src/array/sumOfElement.java
@@ -1,4 +1,12 @@
 package array;
 
 public class sumOfElement {
+    public static void main(String[] args) {
+        int number[] = {23, 45,56,67,87, 89};
+        int sum = 0;
+        for(int temp : number) {
+            sum+=temp;
+        }
+        System.out.println("Total sum is " + sum);
+    }
 }
