b94d2f753ce9cb704dee226cc4388fb4a090c23e
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-23 23:16:39 +0530

    string

--- src/string.java
+++ src/string.java
@@ -1,2 +1,9 @@
 public class string {
+    public static void main(String[] args) {
+        String name = "Shubham";
+        String newname = new String("Shubham");
+        System.out.println(name);
+        System.out.println(newname);
+
+    }
 }
