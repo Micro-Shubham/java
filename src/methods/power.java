ad3151abc82e4ed67c138433eceb3dad9d267357
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-23 22:52:45 +0530

    power

--- src/methods/power.java
+++ src/methods/power.java
@@ -1,4 +1,9 @@
 package methods;
 
 public class power {
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 5;
+        System.out.println(Math.pow(firstNumber,secondNumber));
+    }
 }
