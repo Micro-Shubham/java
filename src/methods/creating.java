7fbb319a68aac806212bc2716ba3ee7ec7b909c7
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-23 23:05:04 +0530

    random

--- src/methods/creating.java
+++ src/methods/creating.java
@@ -1,4 +1,11 @@
 package methods;
 
 public class creating {
+    public static void main(String[] args) {
+        System.out.println(createRandom());
+    }
+    public static int createRandom () {
+        int data = (int) (Math.random()*6);
+        return data;
+    }
 }
