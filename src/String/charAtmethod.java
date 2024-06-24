3c5bd2954080da76f584a8dab78220192a51f309
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:45:30 +0530

    charAt

--- src/String/charAtmethod.java
+++ src/String/charAtmethod.java
@@ -1,4 +1,8 @@
 package String;
 
 public class charAtmethod {
+    public static void main(String[] args) {
+        //charAt provide the index value.
+        System.out.println("billion".charAt(4));
+    }
 }
