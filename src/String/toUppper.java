59e760371451f42a329e19d5965a7569636c625f
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:35:53 +0530

    lowercase

--- src/String/toUppper.java
+++ src/String/toUppper.java
@@ -4,5 +4,7 @@ public class toUppper {
     public static void main(String[] args) {
         String name = "shubham";
         System.out.println(name.toUpperCase());
+        String lastName = "Yadav";
+        System.out.println(lastName.toLowerCase());
     }
 }
