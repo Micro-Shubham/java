8d06c9a0ac6d7c5dade325b3291ed0876e09d547
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 18:06:40 +0530

    replace

--- src/String/replaceMethod.java
+++ src/String/replaceMethod.java
@@ -1,4 +1,10 @@
 package String;
 
 public class replaceMethod {
+    public static void main(String[] args) {
+        String words = "India will win this world cup ";
+        String newWords = words.replace("India", "Bharat");
+        System.out.println(words);
+        System.out.println(newWords);
+    }
 }
