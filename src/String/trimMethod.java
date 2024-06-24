73c40c3bd8e4c95a9ccefe84b000daa33b76733b
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:38:17 +0530

    trim method

--- src/String/trimMethod.java
+++ src/String/trimMethod.java
@@ -1,4 +1,10 @@
 package String;
 
 public class trimMethod {
+    public static void main(String[] args) {
+        String name = "     asjdflaskjfklj    ";
+        //trim method is use to remove extra spaces
+        System.out.println("Before"+name);
+        System.out.println("After " + name.trim());
+    }
 }
