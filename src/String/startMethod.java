f709fd018fa8fbfbd3813ff6f92a3851d1fb047b
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:42:48 +0530

    endswith

--- src/String/startMethod.java
+++ src/String/startMethod.java
@@ -5,5 +5,7 @@ public class startMethod {
         String name = "billionair";
         //startwith method in string helps to find that those words are present or not in boolean .
         System.out.println(name.startsWith("billion"));
+        //for ending words use endwith
+        System.out.println(name.endsWith("r"));
     }
 }
