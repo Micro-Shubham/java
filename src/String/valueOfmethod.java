7f6caa89877a227edfeb1aede80199d54f6513ac
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:49:08 +0530

    valueOf

--- src/String/valueOfmethod.java
+++ src/String/valueOfmethod.java
@@ -1,4 +1,11 @@
 package String;
 
 public class valueOfmethod {
+    public static void main(String[] args) {
+        int number = 334234;
+        //to treate any value as a string use valueOf
+        System.out.println(String.valueOf(number));
+        System.out.println(number);
+
+    }
 }
