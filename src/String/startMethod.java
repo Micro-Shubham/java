766ceb82a32afddfc273ecfaf2a2755c1cc4d416
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-24 17:41:37 +0530

    startwith

--- src/String/startMethod.java
+++ src/String/startMethod.java
@@ -1,4 +1,9 @@
 package String;
 
 public class startMethod {
+    public static void main(String[] args) {
+        String name = "billionair";
+        //startwith method in string helps to find that those words are present or not in boolean .
+        System.out.println(name.startsWith("billion"));
+    }
 }
