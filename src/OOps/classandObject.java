8656ff7f3aea4b778029299f0f610c37c80714af
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-06-26 22:45:35 +0530

    class and object

--- src/OOps/classandObject.java
+++ src/OOps/classandObject.java
@@ -1,4 +1,27 @@
 package OOps;
 
 public class classandObject {
+    public static void main(String[] args) {
+        //creating object
+        dog d1 = new dog();
+        d1.age = 1;
+        d1.legs = 4;
+        d1.bark();
+        d1.walk();
+
+    }
+}
+//create a class
+class dog  {
+    //make some property
+    String name;
+    int age ;
+    int legs;
+    //behaiviour
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+    void walk() {
+        System.out.println("The dog walk on four legs ");
+    }
 }
